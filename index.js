e33d69d25a97584684b41ba1e06568f2cfc3b9df
Manuel Mandatori <manuel.mandatori@lastminute.com>
2018-11-07 23:03:48 +0100

    clicking on anchor when clicking on button

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('[data-name]').click(
         function () {
             var target = $(this).data('name');
-            console.log(target)
-            $('[data-' + target + ']').tab('show')
+            $('a.'+target).click();
+            // $('[data-' + target + ']').tab('show')
         }
     )
 });
