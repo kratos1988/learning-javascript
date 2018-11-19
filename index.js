b7ada94634fcd1ffbd9b876ad4f738123068eea9
Manuel Mandatori <manuel.mandatori@lastminute.com>
2018-11-19 22:13:36 +0100

    Removing use of jquery

--- index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('[data-name]').click(
-        function () {
-            var target = $(this).data('name');
-            $('a.'+target).click();
-            // $('[data-' + target + ']').tab('show')
-        }
-    )
-});
